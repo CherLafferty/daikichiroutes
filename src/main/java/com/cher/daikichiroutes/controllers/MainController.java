6f329b846a16170228751c03bb55083939dcef8c
Cher Lafferty <cherlafferty01@gmail.com>
2022-02-12 12:33:06 -0800

    second commit

--- src/main/java/com/cher/daikichiroutes/controllers/MainController.java
+++ src/main/java/com/cher/daikichiroutes/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.cher.daikichiroutes.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,21 @@ public class MainController {
 	public String tomorrow() {
 		return "<h1>Tomorrow an opportunity will arise, so be sure to be open to new ideas.</h1>";
 	}
+	
+	@RequestMapping("/travel/{city}")
+	public String showCity(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city + "!";
+	}
+	
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") Integer num) {
+		if(num % 2 == 0) {
+		return "You will take a grand journey in the near future, but be wary of tempting offers.";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend tiem with family and friends.";
+		}
+	}
+	
 	
 	
 }
